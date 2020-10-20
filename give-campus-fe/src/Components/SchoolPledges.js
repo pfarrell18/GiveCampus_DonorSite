14ea55deb9f0a8fd25ab691d986cd0c4f44ad777
Priyanka Farrell <priyanka@Nasrs-MacBook-Pro.local>
2020-10-20 15:48:25 -0400

    adding single page

--- give-campus-fe/src/Components/SchoolPledges.js
+++ give-campus-fe/src/Components/SchoolPledges.js
@@ -1,27 +1,28 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
+import {Link} from "react-router-dom"
 import FormatDate from "../Helper Functions/formatDate"
 
 const SchoolPledges = () => {
     const [formData, setFormData] = useState({})
-    const [pledges, setPledges]=useState(undefined)
+    const [pledges, setPledges] = useState(undefined)
 
     const handleChange = (evt) => {
         setFormData(evt.target.value)
     }
 
     const handleSubmit = async (evt) => {
-        
+
         evt.preventDefault();
         const settings = {
             method: 'POST',
-            body: JSON.stringify({"institution": formData}),
+            body: JSON.stringify({ "institution": formData }),
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             }
         };
         const fetchResponse = await fetch("/findschoolpledges", settings)
-        const data= await fetchResponse.json()
+        const data = await fetchResponse.json()
         setPledges(data)
     }
 
@@ -33,7 +34,7 @@ const SchoolPledges = () => {
             <form onSubmit={handleSubmit}>
                 <div className="field">
                     <div className="control">
-                        <input className="input" type="text" id="institution" placeholder="Enter Institution Name" onChange = {handleChange} required/>
+                        <input className="input" type="text" id="institution" placeholder="Enter Institution Name" onChange={handleChange} required />
                     </div>
                 </div>
                 <div className="field is-grouped is-grouped-centered">
@@ -45,19 +46,19 @@ const SchoolPledges = () => {
                     </div>
                 </div>
             </form>
+            <h1 className="title">Pledges</h1>
             <div className="card-container">
-            {pledges && pledges.map((pledge, idx)=>{
-                return(<div className={`card ${pledge.expired && "expired"}`} key={idx}>
-                    <div className="subtitle">{pledge.institution}</div>
-                    <div className="subtitle">{pledge.name}</div>
-                    <div>Start Date: 
-                    <FormatDate date={pledge.start_date}/>
-                    </div>
-                    <div>End Date: 
-                    <FormatDate date={pledge.end_date}/>
-                    </div>
+                {pledges && pledges.sort((a, b) => {return new Date(b.end_date) - new Date(a.end_date)}).filter((item)=>{return !item.expired}).map((pledge, idx) => {
+                    return (<div className={`card ${pledge.expired && "expired"}`} key={idx}>
+                        <div className="subtitle">{pledge.institution}</div>
+                        <div className="subtitle">{pledge.name}</div>
+                        <div>End Date:
+                            <FormatDate date={pledge.end_date} />
+                        </div>
+                        <hr></hr>
+                        <Link to={{pathname:"/singlepledge", pledge: pledge }} className="button">See More</Link>
                     </div>)
-            })}
+                })}
             </div>
         </div>
     )
